0fd0e5b2478ccb2cd0a7df977669600a9badf1ae
Luan Alves de Souza <a15910a@gmail.com>
2024-11-12 11:21:29 -0300

    Adividade 05

---
+++ src/main/java/br/com/ifba/atividade005/veiw/Funcao01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade005.veiw;
+
+/**
+ *
+ * @author a1591
+ */
+public class Funcao01 {
+    
+    public static void main(String[] args){
+        Fatorial f = new Fatorial();
+        
+        f.setValor(5);
+        System.out.printf(f.getFormula());
+        System.out.println(f.getFatorial());
+    }
+    
+    
+}
